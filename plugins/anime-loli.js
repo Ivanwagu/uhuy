fc1821f3d7d39ae2d9161ba34c5a2785705ada4b
Icontol <119477979+Icontol@users.noreply.github.com>
2023-02-08 19:57:23 +0700

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://web-production-7c28.up.railway.app/api/wallpaper/loli?apikey=APIKEY'
-	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
+	let url = 'https://raw.githubusercontent.com/Icontol/kontolbapakkaupecah/master/null-20230205-WA0075.jpg'
+	conn.sendButton(m.chat, 'ngga ada loli dasar pedo', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(loli)$/i
 handler.tags = ['anime']
